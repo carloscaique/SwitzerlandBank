180259127616af32b403251555912f29d1c13058
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2023-05-25 17:30:10 -0300

    add & fix - dados.html and fix its links

---
+++ assets/scripts/aviso.js
@@ -0,0 +1,9 @@
+function exibirAviso() {
+    var aviso = document.getElementById("aviso");
+    aviso.classList.remove("escondido");
+  }
+
+  function fecharAviso() {
+    var aviso = document.getElementById("aviso");
+    aviso.classList.add("escondido");
+  }
